70b889fce62a1836e2e89fd5f17c5f01165130da
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2025-01-14 16:13:40 +0300

    Sign-In Controller added

---
+++ BaseProject/IdentityBase/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using IdentityBase.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+
+namespace IdentityBase.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AccountController(UserManager<AppUser> _userManager, SignInManager<AppUser> _signInManager)
+        {
+            this._userManager = _userManager;
+            this._signInManager = _signInManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var hasUser = await _userManager.FindByEmailAsync(email);
+            if(hasUser == null)
+            {
+                return View();
+            }
+            var result = await _signInManager.PasswordSignInAsync(hasUser, password, true, false);
+            if (!result.Succeeded)
+            {
+                return View();
+            }
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
